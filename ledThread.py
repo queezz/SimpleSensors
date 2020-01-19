1eff03f19b20bc1e805daa8d710f81cf49a7075d
queezz <queezz@gmail.com>
2020-01-20 01:17:58 +0900

    added blinking in ledThread.py

--- ledThread.py
+++ ledThread.py
@@ -13,6 +13,36 @@ class LED(threading.Thread):
         self.__off = False
         self.__pin = pin
         self.setup(duty,period)
+        self.blink_setup()
+
+    def blink_setup(self,durs = [0.05,0.05]):
+        """ setup LED blinking times and state. durs = [time on, time off] """
+        self.blink_on= False # blink = True, do not blink = False 
+        self.blink_state = 1 # LED ON = 1, LED OFF = 0
+        self.blink_durs = durs
+        self.blink_t = self.blink_durs[0]
+
+    def blink_changestate(self):
+        """ change blink state between ON and OFF"""
+        if self.blink_state:
+            self.blink_t = self.blink_durs[1]
+            self.blink_state = 0
+            self.blink_timer = time.time()
+        else:
+            self.blink_t = self.blink_durs[0]
+            self.blink_state = 1
+            self.blink_timer = time.time()
+
+    def blink_start(self):
+        """ start to blink """
+        self.blink_on = True
+        self.blink_state = 1
+        self.blink_timer = time.time()
+
+    def blink_stop(self):
+        """ start to blink """
+        self.blink_state = 1 # used in PWD condition, must be 1
+        self.blink_on = False 
 
     def stop(self):
         """ Turn OFF LED and stop main loop """
@@ -46,7 +76,11 @@ class LED(threading.Thread):
 
         while not self.__stop:
             # if self.duty == 0, don't send voltage to LED
-            if not self.__off:
+            if self.blink_on:
+                if time.time()-self.blink_timer > self.blink_t:
+                    self.blink_changestate()
+
+            if not self.__off and self.blink_state:
                 GPIO.output(CH,1)
                 time.sleep(self.t1)
             else:
@@ -129,10 +163,34 @@ def test_change_duty():
 
 def test_blink():
     """ blink one LED """
+    CHS = [21,20,16]
+    GPIO.setmode(GPIO.BCM)
+    [GPIO.setup(CH,GPIO.OUT) for CH in CHS]
+    period = 0.005
+    duties = [0.3,0.05,0.3]
+    dur = 1
+    nms = ['red','green','blue']
+    leds  = {n:LED(CH,duty,period) for CH,duty,n in zip(CHS,duties,nms)}
+    
+    [led.blink_setup(durs=[0.02,0.08]) for led in leds.values()]
+    [led.blink_start() for led in leds.values()]
+    [led.start() for led in leds.values()]
+
+    time.sleep(dur)
+    [led.blink_stop() for led in leds.values()]
+    time.sleep(dur)
+    
+    [led.blink_start() for led in leds.values()]
+    time.sleep(dur)
+    
+    [led.stop() for led in leds.values()]
+    # wait for all threads to finish
+    [led.join() for led in leds.values()]
+    GPIO.cleanup()
 
 if __name__ == "__main__":
     ""
     #test_one_led()
     #test_several()
-    test_change_duty()
-    #test_blink()
+    #test_change_duty()
+    test_blink()
